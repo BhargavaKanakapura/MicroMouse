2454fc624d38f1e16b4e9ae5e3004503fd9cb1c7
BhargavaKanakapura <77862840+BhargavaKanakapura@users.noreply.github.com>
2022-01-21 22:03:21 -0800

    Create compiler.js

---
+++ Micro_Mouse_Sim/scripts/modules/compiler.js
@@ -0,0 +1,29 @@
+/*
+The purpose of this file to control which functions and variables the user can use
+*/
+
+import {
+	moveForward, 
+	turnRight, 
+	turnLeft, 
+	isWallInFront, 
+	isWallLeft, 
+	isWallRight, 
+	corridorLength, 
+	paths, 
+	foundFinish, 
+	resetCount
+} from "./micromouse.js";
+
+import {Graph} from "./tools/dsgraph.js";
+import {Queue} from "./tools/dsqueue.js";
+import {printUI} from "/Micro_Mouse_Sim/sources/scripts/script.js"
+
+export function compile(code) {
+	/*
+	Evaluate the code
+	*/
+	
+	eval(code);
+	
+}
