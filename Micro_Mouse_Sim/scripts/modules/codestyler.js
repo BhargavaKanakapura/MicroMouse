1c971e743518e4adba2a2b3b7ca7a3a7e71a9431
BhargavaKanakapura <77862840+BhargavaKanakapura@users.noreply.github.com>
2022-01-29 22:44:04 -0800

    Update codestyler.js

--- Micro_Mouse_Sim/scripts/modules/codestyler.js
+++ Micro_Mouse_Sim/scripts/modules/codestyler.js
@@ -1,16 +1,43 @@
-const conditionals = ['if', 'else', 'switch', 'case', 'try', 'catch', 'throw', 'finally'];
-const declarations = ['var', 'let', 'const', 'function', 'class'];
-const keywords = ['this', 'window', 'async', 'true', 'false', 'undefined', 'for', 'while', 'continue'];
-const separators = ['(', ')', '!', '?', '$', '#', ':', '{', '}', ';', '?', '+', '-', '*', '/', ',', '.', '^', '&', '|', '[', ']', '=', '<', '>', ' '];
-
-
-export function styleCode() {
+function styleCode(elem) {
 	/*
 	Style and format code to display
 	*/
+
+}
+
+export function styleMMAI() {
+	/*
+	Style the minimixed MMAI text editor
+	*/
 	
-	let text = document.getElementById('mmai').innerHTML;
-	//console.log(text);
+	styleCode(document.getElementById('mmai'));
+	
+}
+
+export function styleHeader() {
+	/*
+	Style the minimixed header text editor
+	*/
+	
+	styleCode(document.getElementById('header'));
+	
+}
+
+export function styleMaxMMAI() {
+	/*
+	Style the maximixed MMAI text editor
+	*/
+	
+	styleCode(document.getElementById('expandedmmai'));
+	
+}
+
+export function styleMaxHeader() {
+	/*
+	Style the maximixed header text editor
+	*/
+	
+	styleCode(document.getElementById('expandedheader'));
 	
 }
 
@@ -19,11 +46,31 @@ export function standardizeCode(codeAsText) {
 	Remove all HTML tags and format as js code+
 	*/
 	
-	return codeAsText
-		.replaceAll('\n', ' ')   //Replace new lines
-		.replaceAll('&lt;', '<') //Replace HTML chars
-		.replaceAll('&gt;', '>')
-		.replaceAll('&amp;', '&')
-		.replaceAll(/(<([^>]+)>)/ig, ''); //Remove HTML tags
+	//Remove comments
+	let singleLineComment = /\/\/.*?\n/g;  //RegExp for // ... \n
+	let multiLineComment = /\/\*.*?\*\//g; //RegExp for /* ... */
+	
+	//Remove HTML tags
+	let htmlTag = /(<([^>]+)>)/ig;
+	
+	//Replace HTML entities
+	let gt = '&gt;';
+	let lt = '&lt;';
+	let and = '&amp;';
+	
+	//Replace new lines
+	let newLine = '\n'
+	
+	codeAsText = codeAsText
+		.replaceAll(singleLineComment, ' ') //Replace single line comments sourrounded by // and \n
+		.replaceAll(multiLineComment, ' ')  //Replace multi line comments sourrounded by /* and */
+		.replaceAll(newLine, ' ')			//Replace new lines marked by \n
+		.replaceAll(lt, '<')				//Replace the < HTML entity
+		.replaceAll(gt, '>')				//Replace the > HTML entity
+		.replaceAll(and, '&')				//Replace the & HTML entity
+		.replaceAll(htmlTag, ''); 			//Remove HTML tags
+	
+	console.log(codeAsText);
+	return codeAsText;
 	
 }
